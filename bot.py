2bbca5a6eef308e4c3d88e32860e7e8421b3d2b6
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2020-04-25 21:54:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -275,9 +275,9 @@ def createDriver():
     capa["pageLoadStrategy"] = "none"
     chrome_options = ChromeOptions()
     # chrome_options.add_argument("--start-maximized")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     # chrome_options.add_argument("--headless")
     chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_experimental_option('prefs', {
